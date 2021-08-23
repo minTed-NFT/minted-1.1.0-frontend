7100af8be6ff4d41dd1ced6105535fd8e8097461
Fahimansari <fahimpjan@gmail.com>
2021-08-23 22:52:08 +0530

    Layout of Home Page Modified Successfully

---
+++ src/Components/Banner.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Banner = () => {
+    return <div className='banner'>Discover & Collect
+                <br />Extraordinary NFTs From
+                <br />Bitclout
+            </div>
+}
+
+export default Banner
--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -1,18 +1,22 @@
 import React, {Component} from "react";
-
 import { useEffect,useState} from "react";
 import axios from 'axios';
+
 import Creators from "../Components/Creators";
 import Post from "../Components/Post";
+import Banner from '../Components/Banner'
 const Main = ()=> {
 
 
     console.log('I am working');
     return (
-    <section>
-        <Creators />
-        <Post />
-    </section>
+    <main>
+        <div className='container' id='home'>
+            <Banner />
+            <Creators />
+            <Post />
+        </div>
+    </main>
     )
 
 }
