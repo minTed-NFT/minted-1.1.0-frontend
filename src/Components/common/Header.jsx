d55c39be233c3f653b933005f10e6f21c9f8514d
Prakash C <prakashchidambaram2001@gmail.com>
2021-09-17 19:58:41 +0530

    data using get method

--- src/Components/common/Header.jsx
+++ src/Components/common/Header.jsx
@@ -4,67 +4,70 @@ import SearchBar from './searchbar';
 import axios from 'axios';
 const Header = () => {
 
-    const [loggedIn, setlogin] = useState(false)
-    const [userdp , setuserdp] = useState('')
-    const [user,setuser] = useState('a')
-    const identityURL = 'https://identity.bitclout.com'
-    let identityWindow
-    let userPayload
-    let dp
-    let usernow
-    let data
+	const [loggedIn, setlogin] = useState(false)
+	const [userdp , setuserdp] = useState('')
+	const [user,setuser] = useState('a')
+	const identityURL = 'https://identity.bitclout.com'
+	let identityWindow
+	let userPayload
+	let dp
+	let usernow
+	let data
 
-    const launchLogin = () => {
-        const h = 1000;
-        const w = 800;
-        const y = window.outerHeight/2 + window.screenY - h/2;
-        const x = window.outerWidth/2 + window.screenX - w/2; 
+	const launchLogin = () => {
+		const h = 1000;
+		const w = 800;
+		const y = window.outerHeight/2 + window.screenY - h/2;
+		const x = window.outerWidth/2 + window.screenX - w/2; 
 
-        window.addEventListener('message', e => {
-            console.log('This is event');
-            console.log(e);
+		window.addEventListener('message', e => {
+		//console.log('This is event');
+		//console.log(e);
 
-            if (e.data && e.origin) {
-                if (e.origin === 'https://identity.bitclout.com') {
-                    if (e.data.method === 'initialize') {
-                        //console.log('initialize');
-                        const data = {id: e.data.id, service: 'identity'};
-                        //console.log('this is data');
-                        //console.log(data);
-                        identityWindow.postMessage(data, '*');
-                    }
+		if (e.data && e.origin) {
+			if (e.origin === 'https://identity.bitclout.com') {
+				if (e.data.method === 'initialize') {
+				//console.log('initialize');
+				const data = {id: e.data.id, service: 'identity'};
+				//console.log('this is data');
+				//console.log(data);
+				identityWindow.postMessage(data, '*');
+				}
 
-                    if (e.data.method == 'login' ) {
-                        console.log('login');
-                        console.log(e.data);
-                        	usernow = e.data.payload.publicKeyAdded
-                        	console.log(user)
-                        	setuser(usernow)
-                        	console.log(user)
-                        	dp = "https://bitclout.com/api/v0/get-single-profile-picture/"+ usernow +"?fallback=https://bitclout.com/assets/img/default_profile_pic.png"
-                        	setuserdp(dp)
-				        console.log(user);
-				        userPayload = e.data.payload.users[user]
-				        console.log(userPayload)
-				        identityWindow.close()
-				        setlogin(true)
-				    console.log(user)
+				if (e.data.method == 'login' ) {
+					//console.log('login');
+					//console.log(e.data);
+					usernow = e.data.payload.publicKeyAdded
+					//console.log(user)
+					setuser(usernow)
+					//console.log(user)
+					dp = "https://bitclout.com/api/v0/get-single-profile-picture/"+ usernow +"?fallback=https://bitclout.com/assets/img/default_profile_pic.png"
+					setuserdp(dp)
+					//console.log(user);
+					userPayload = e.data.payload.users[user]
+					//console.log(userPayload)
+					identityWindow.close()
+					setlogin(true)
+					//console.log(user)
     					data = { 'publickey' : usernow }
-    					console.log('this is data-axios')
-    					console.log(data)
+    					//console.log('this is data-axios')
+    					//console.log(data)
     					axios.get(`http://localhost:5001/userlog/${usernow}`)
-                    }
-            }
-            }
+					}
+				}
+			}
 
-        })
+		})
 
-        identityWindow = window.open(identityURL + '/log-in', null, `toolbar=no, width=${w}, height=${h}, top=${y}, left=${x}`);
+		identityWindow = window.open(identityURL + '/log-in', null, `toolbar=no, width=${w}, height=${h}, top=${y}, left=${x}`);
     }
     const launchLogout = () => {
     		setlogin(false)
     		setuserdp('')
     		setuser('a')
+    		usernow = ''
+    		axios.get(`http://localhost:5001/userlog/${usernow}`)
+    		
     		
     		}
     
@@ -90,9 +93,10 @@ const Header = () => {
                 </ul>
             </nav>
         </header>
-    )
-    }
+    )}
+    
     else{
+    
     return (
         <header>
             <nav className="navbar navbar-custom">
